82d8564b78586843b31640d3689636f48a6d4720
Juan C Peralta <juanc7peralta@gmail.com>
2025-11-20 09:14:28 -0300

    implemented BACKEND SPRING(JAVA) (BETA) (Estructure)

---
+++ backend/src/main/java/initCine.java
@@ -0,0 +1,11 @@
+package src.main.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class initCine {
+    public static void main(String[] args) {
+        SpringApplication.run(initCine.class, args);
+    }
+}
