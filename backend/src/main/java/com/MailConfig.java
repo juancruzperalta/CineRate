29c554cfc4a457d3ce3d05aa82319f0fcfd3dad0
Juan C Peralta <juanc7peralta@gmail.com>
2026-01-27 19:27:11 -0300

    change pom

---
+++ backend/src/main/java/com/MailConfig.java
@@ -0,0 +1,28 @@
+package com;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+  @Bean
+  public JavaMailSender javaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost("smtp.gmail.com");
+    mailSender.setPort(465);
+    mailSender.setUsername("juanpera3000@gmail.com");
+    mailSender.setPassword("qdfd dkhd gbit izdc");
+    Properties props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
+
+    return mailSender;
+  }
+}
