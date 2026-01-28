15621d70de4631637826e394d6404254672c55e4
Juan C Peralta <juanc7peralta@gmail.com>
2026-01-28 17:03:02 -0300

    Problem with the password and user for emailSender

--- backend/src/main/java/com/MailConfig.java
+++ backend/src/main/java/com/MailConfig.java
@@ -2,6 +2,7 @@ package com;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,18 +10,23 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailConfig {
+  @Value("${MAIL_USER}")
+  private String mailUser;
 
+  @Value("${MAIL_PASS}")
+  private String mailPass;
   @Bean
   public JavaMailSender javaMailSender() {
     JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
     mailSender.setHost("smtp.gmail.com");
-    mailSender.setPort(465);
-    mailSender.setUsername("juanpera3000@gmail.com");
-    mailSender.setPassword("qdfd dkhd gbit izdc");
+    mailSender.setUsername(mailUser);
+    mailSender.setPort(587);
+    mailSender.setPassword(mailPass);
     Properties props = mailSender.getJavaMailProperties();
     props.put("mail.transport.protocol", "smtp");
     props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.ssl.enable", "false");
     props.put("mail.debug", "true");
 
     return mailSender;
