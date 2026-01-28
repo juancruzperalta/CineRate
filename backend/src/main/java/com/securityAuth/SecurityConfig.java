783f086f97b95dc8d5dd309c1ff2667c20d70a7a
Juan C Peralta <juanc7peralta@gmail.com>
2026-01-28 16:28:45 -0300

    Cors has been add?

--- backend/src/main/java/com/securityAuth/SecurityConfig.java
+++ backend/src/main/java/com/securityAuth/SecurityConfig.java
@@ -12,29 +12,29 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
   private final JWTAuthFilter jwtAuthFilter;
+   private final CorsConfigurationSource corsConfigurationSource;
+
+    public SecurityConfig(CorsConfigurationSource corsConfigurationSource, JWTAuthFilter jwtAuthFilter) {
+        this.corsConfigurationSource = corsConfigurationSource;
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    public SecurityConfig(JWTAuthFilter jwtAuthFilter) {
-        this.jwtAuthFilter = jwtAuthFilter;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
       http
           .csrf(csrf -> csrf.disable())
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .cors(cors -> cors.configurationSource(corsConfigurationSource))
           .authorizeHttpRequests(auth -> auth
           .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
           .requestMatchers(
