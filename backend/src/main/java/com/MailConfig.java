b50c9506dbfb04b12df736aad7b86fe0ad641409
Juan C Peralta <juanc7peralta@gmail.com>
2026-01-28 18:41:08 -0300

    Modified mail config

--- backend/src/main/java/com/MailConfig.java
+++ backend/src/main/java/com/MailConfig.java
@@ -2,9 +2,12 @@ package com;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -26,10 +29,24 @@ public class MailConfig {
     Properties props = mailSender.getJavaMailProperties();
     props.put("mail.transport.protocol", "smtp");
     props.put("mail.smtp.auth", "true");
-      props.put("mail.smtp.starttls.enable", "true");
-      props.put("mail.smtp.ssl.enable", "false");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.smtp.ssl.enable", "false");
     props.put("mail.debug", "true");
 
     return mailSender;
   }
+  @Autowired
+private JavaMailSender mailSender;
+
+@Bean
+CommandLineRunner testMail() {
+    return args -> {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("juanpera3000@gmail.com");
+        msg.setSubject("Test Mail");
+        msg.setText("Funciona en Railway!");
+        mailSender.send(msg);
+        System.out.println("Mail enviado!");
+    };
+}
 }
--- backend/src/main/java/com/securityAuth/SecurityConfig.java
+++ backend/src/main/java/com/securityAuth/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
       http
-      .cors(cors -> cors.configurationSource(corsConfigurationSource))
+          .cors(cors -> cors.configurationSource(corsConfigurationSource))
           .csrf(csrf -> csrf.disable())
           .authorizeHttpRequests(auth -> auth
           .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
