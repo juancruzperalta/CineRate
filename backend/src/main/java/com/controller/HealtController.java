ed4faa00a8892232b085dd1d7eca8a9c7ad49d9f
Juan Cruz Peralta <juanc7peralta@gmail.com>
2026-02-24 10:30:14 -0300

    Healt add for backend.

---
+++ backend/src/main/java/com/controller/HealtController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealtController{
+    @GetMapping
+    public ResponseEntity<String> check(){
+        return ResponseEntity.ok("Ok");
+    }
+}
